2c16c17a7e999bd2a5747f4f74a6af6473fdab10
Rishi Balamurugan <45452035+Crazzygamerr@users.noreply.github.com>
2023-02-10 21:21:19 +0530

    chore: Cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,71 +11,8 @@ import rightArrow from "../../public/right-arrow.svg";
 import "../styles/routes/index.scss";
 import Head from "next/head";
 
-function Home({ props, }: { props: any }): JSX.Element {
+function Home(): JSX.Element {
 	
-
-	// function GetUserUid() {
-	//   const [uid, setUid] = useState(null);
-	//   useEffect(() => {
-	//     auth.onAuthStateChanged((user) => {
-	//       if (user) {
-	//         setUid(user.uid);
-	//       }
-	//     });
-	//   }, []);
-	//   return uid;
-	// }
-
-	// const uid = GetUserUid();
-
-	//getting a current user function
-	// function GetCurrentUser() {
-	//   const [user, setUser] = useState(null);
-	//   useEffect(() => {
-	//     auth.onAuthStateChanged((user) => {
-	//       if (user) {
-	//         fs.collection("users")
-	//           .doc(user.uid)
-	//           .get()
-	//           .then((snapshot) => {
-	//             setUser(snapshot.data().FullName);
-	//           });
-	//       } else {
-	//         setUser(null);
-	//       }
-	//     });
-	//   }, []);
-	//   return user;
-	// }
-
-	// const user = GetCurrentUser();
-	// console.log(user)
-
-	//state of products
-	// const [products, setProducts] = useState([]);
-
-	//Getting products function
-	// const getProducts = async () => {
-	//   const products = await fs.collection("products").get();
-	//   const productsArray = [];
-	//   for (var snap of products.docs) {
-	//     var data = snap.data();
-	//     data.ID = snap.id;
-	//     productsArray.push({
-	//       ...data,
-	//     });
-	//     if (productsArray.length === products.docs.length) {
-	//       setProducts(productsArray);
-	//     }
-	//   }
-	// };
-
-	// console.log(products.length);
-
-	// useEffect(() => {
-	//   getProducts();
-	// }, []);
-
 	return (
 		<div className="Landing">
 			<Head>
