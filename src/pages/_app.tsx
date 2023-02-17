b37ca43b379a9945ea8df832d7a019b1d94a6948
Rishi Balamurugan <45452035+Crazzygamerr@users.noreply.github.com>
2023-02-18 00:28:31 +0530

    update: Font change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,22 +2,21 @@ import '@/styles/root/globals.scss'
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import type { AppProps } from 'next/app'
-import { Alegreya } from "@next/font/google";
+import { Maven_Pro } from "@next/font/google";
 import Navbar from '@/components/Navbar/Navbar';
 import Footer from '@/components/Footer/Footer';
 import { SessionProvider } from 'next-auth/react';
 
-const alegreya = Alegreya({
+const maven_Pro = Maven_Pro({
 	subsets: ["latin"],
 });
-
 const App: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
 	return (
 		<SessionProvider session={session}>
-			<main className={alegreya.className}>
+			<main className={maven_Pro.className}>
 				<Navbar user={null} />
 				<Component {...pageProps} />
 				<Footer />
